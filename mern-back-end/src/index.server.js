480f1c6535b1e7c24c3ef78793ec7a344262d445
Himanshunitrr <himanshumauryacrz@gmail.com>
2020-09-16 01:13:34 +0530

    ...

--- mern-back-end/src/index.server.js
+++ mern-back-end/src/index.server.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 // routes
 const authRoutes = require("./routes/auth")
 const adminRoutes = require("./routes/admin/auth")
-
+const categoryRoutes = require("./routes/category")
 env.config();
 
 mongoose.connect(`mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.bou85.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`, {
@@ -19,6 +19,7 @@ mongoose.connect(`mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO
 app.use(express.json());
 app.use("/api", authRoutes)
 app.use("/api", adminRoutes)
+app.use("/api", categoryRoutes)
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
